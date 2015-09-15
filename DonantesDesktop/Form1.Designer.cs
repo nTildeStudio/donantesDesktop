36f747d085f718ff2a8df556e9f5863239265fde
Carlos Domínguez <carlos.sevilla@gmail.com>
2015-09-15 10:16:12 +0200

    creación y prueba de proyecto

---
+++ DonantesDesktop/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace DonantesDesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btPush = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbMsg = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbDestino = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btPush
+            // 
+            this.btPush.Location = new System.Drawing.Point(12, 58);
+            this.btPush.Name = "btPush";
+            this.btPush.Size = new System.Drawing.Size(402, 23);
+            this.btPush.TabIndex = 0;
+            this.btPush.Text = "Push";
+            this.btPush.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mensaje";
+            // 
+            // tbMsg
+            // 
+            this.tbMsg.Location = new System.Drawing.Point(82, 6);
+            this.tbMsg.Name = "tbMsg";
+            this.tbMsg.Size = new System.Drawing.Size(332, 20);
+            this.tbMsg.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Destinatario";
+            // 
+            // tbDestino
+            // 
+            this.tbDestino.Location = new System.Drawing.Point(82, 32);
+            this.tbDestino.Name = "tbDestino";
+            this.tbDestino.Size = new System.Drawing.Size(332, 20);
+            this.tbDestino.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(422, 86);
+            this.Controls.Add(this.tbDestino);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbMsg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btPush);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btPush;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbMsg;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbDestino;
+    }
+}
+
---
+++ DonantesDesktop/Form1.cs
@@ -0,0 +1,41 @@
+﻿using Parse;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DonantesDesktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            ParseClient.Initialize("", "");
+
+            btPush.Click+=new EventHandler(sendPush);
+        }
+
+        async void sendPush(object sender, EventArgs e)
+        {
+            var push = new ParsePush();
+            push.Query = from installation in ParseInstallation.Query
+                         where installation.Get<String>("numeroDonante") == tbDestino.Text
+                         select installation;
+            push.Alert = tbMsg.Text;
+            await push.SendAsync();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
